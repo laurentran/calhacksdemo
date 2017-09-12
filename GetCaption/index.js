6a9bcf1604f0421ee70618dfa7a5df5f8d97bbf8
Lauren Tran <latra@microsoft.com>
2017-09-12 12:24:22 -0700

    add cognitive services computer vision api call

--- GetCaption/index.js
+++ GetCaption/index.js
@@ -1,16 +1,33 @@
+request = require('request')
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+        var url = req.body.name;
+        requestBody = JSON.stringify({"url":url})
+        request.post({
+            headers: {'content-type':'application/json', 'Ocp-Apim-Subscription-Key':process.env.APIKEY},
+            url: 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/describe',
+            body: requestBody,
+        }, function(error, response, body){
+            if(error) {
+                context.log(error);
+            } else {
+                var result = JSON.parse(body).description.captions[0].text;
+                context.log(result);
+                context.res = {
+                // status: 200, /* Defaults to 200 */
+                    body: result
+                };
+            }
+        });
+
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass an imageURL on the query string or in the request body"
         };
     }
     context.done();
