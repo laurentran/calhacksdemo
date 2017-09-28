7700587abc54fbc11792aa768e6aacf90690facb
Lauren Tran <latra@microsoft.com>
2017-09-28 11:47:47 -0700

    rewriting using Node core

--- GetCaption/index.js
+++ GetCaption/index.js
@@ -1,30 +1,13 @@
-request = require('request')
+https = require('https');
 
 module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
+    context.log('***************************** Start Run ****************************')
+    
     if (req.query.imageURL || (req.body && req.body.imageURL)) {
         var url = req.body.imageURL;
-        requestBody = JSON.stringify({"url":url})
-        request.post({
-            headers: {'content-type':'application/json', 'Ocp-Apim-Subscription-Key':process.env.APIKEY},
-            url: 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/describe',
-            body: requestBody,
-        }, function(error, response, body){
-            if(error) {
-                context.log(error);
-            } else {
-                var result = JSON.parse(body).description.captions[0].text;
-                context.log(result);
-                context.res = {
-                // status: 200, /* Defaults to 200 */
-                    body: result
-                };
-                context.done();
-            }
-        });
-    }
-    else {
+        postContent(url, context);
+
+    } else {
         context.res = {
             status: 400,
             body: "Please pass an imageURL on the query string or in the request body"
@@ -32,3 +15,50 @@ module.exports = function (context, req) {
         context.done();
     }
 };
+
+// make request to Cognitive Services
+const postContent = function(url, context) {
+
+    var postData = JSON.stringify({'url': url});
+
+    var options = {
+        hostname: 'westus.api.cognitive.microsoft.com',
+        port: 443,
+        path: '/vision/v1.0/describe',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': process.env.API_KEY
+        }
+    };
+
+    var req = https.request(options, (res) => {
+        context.log(`STATUS: ${res.statusCode}`);
+        res.setEncoding('utf8');
+        
+        res.on('data', (chunk) => {
+            context.log(`BODY: ${chunk}`);
+            var result = JSON.parse(chunk).description.captions[0].text;
+            context.log(result);
+            context.res = {
+                body: result
+            };
+        });
+        
+        res.on('end', () => {
+            context.log('***************************** End Run ****************************');
+            context.done();
+            });
+        });
+
+        req.on('error', (e) => {
+            context.log(`problem with request: ${e.message}`);
+            context.log('***************************** End Run ****************************');
+            context.done();
+        });
+
+    // write data to request body
+    req.write(postData);
+    req.end();
+};
+
